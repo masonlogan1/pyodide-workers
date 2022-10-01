c36287e39524dd570c26a84216fd85e77a574ff9
Mason Logan <masonlogan1@gmail.com>
2022-09-30 22:24:44 -0400

    refactor worker to accept multiple functions for onmessage, remove queue in favor of direct user-defined functions, refactor message methods for readability, add timestamps to WorkerMessage objects

--- pywebworker/worker.py
+++ pywebworker/worker.py
@@ -1,11 +1,12 @@
 import logging
 
+from collections.abc import Iterable
+from datetime import datetime
 from queue import Queue
+import pywebworker.worker_config
 
 # this module is registered in worker_config.py
-from pywebworker import worker_config
 import pywebworker_js
-import js
 
 
 DEFAULT_LOG_LEVEL = logging.DEBUG
@@ -46,17 +47,27 @@ class FatalWorkerError(WorkerError):
 
 class WorkerMessage:
     """Message from a PyWorker web pywebworker"""
-    def __init__(self, data, status=False):
+    def __init__(self, data, status=False, timestamp=datetime.now()):
         # TODO: add datetime for when message was sent
         self.data = data
         self.status = status
+        self.timestamp = timestamp
 
     def read(self):
         self.status = True
         return self.data
 
+    def received(self) -> datetime:
+        return self.timestamp
+
     def is_read(self) -> bool:
         return self.status
+
+    def __str__(self):
+        return f"WorkerMessage{{'" \
+               f"Read:{str(self.status)};" \
+               f"Received:{self.timestamp.strftime('%Y-%m-%d %H:%M:%S.%f')};" \
+               f"}}"
 
 
 class WorkerMessageQueue:
@@ -68,16 +79,17 @@ class WorkerMessageQueue:
 
     def put(self, event):
         """
-        :param event: web worker event
+        :param event: Web worker event
         Converts a web worker event into a WorkerMessage and puts it into a queue
         """
         self.queue.put(WorkerMessage(event.data))
 
-    def get(self, block=True, timeout=None) -> WorkerMessage:
+    def get(self, block=False, timeout=None) -> WorkerMessage:
         """
         :param block: whether to wait for the next message
         :param timeout: time to wait for the next message, if applicable
         :return: the next WorkerMessage in the queue
+        Returns the next item in the Queue. Blocking disabled by default to prevent unintentionally locking the browser
         """
         return self.queue.get(block=block, timeout=timeout)
 
@@ -85,30 +97,40 @@ class WorkerMessageQueue:
 # You may be asking "couldn't you just use pyodide's js module for this?" The short answer is kinda, the long answer
 # is yes, but with a lot more difficulty. Passing messages back and forth can be buggy and in some cases they can be
 # missed. The Worker class wraps a JavaScript object that helps to facilitate stable communication for any inheriting
-# classes
+# classes. This is exceptionally helpful for loading python scripts into workers
 class Worker:
     """
     Creates a browser-friendly, pythonic way of using web workers with pyodide
     """
-    def __init__(self, script, queue=None, loglevel=logging.DEBUG):
+    def __init__(self, script, onmessage_actions=None, loglevel=logging.DEBUG):
         self.logger = logging.getLogger('PyWorker')
         self.logger.setLevel(loglevel)
 
         self.script = script
         self.worker = pywebworker_js(script)
 
-        self.queue = queue
         self.messages = list()
+
+        self.__onmessage_actions = list()
+        self.set_onmessage(onmessage_actions)
+
+    # some of these might seem unnecessary, but they make importing the script and jumping right in MUCH easier
+    def has_unread_messages(self) -> bool:
+        """
+        Returns True if there are any unread messages
+        """
+        return self.get_unread_messages() != list()
 
     def get_unread_messages(self) -> list[WorkerMessage]:
         """adds any missing messages to the message repository and returns anything not marked as opened"""
         return [message for message in self.messages if not message.is_read()]
 
-    def has_unread_messages(self) -> bool:
+    def get_next_unread_message(self) -> WorkerMessage:
         """
-        Returns True if there are any unread messages
+        :return: the next unread message from the list, if one exists
+        Provides the next unread message. If one does not exist, returns None
         """
-        return any([not message.is_read() for message in self.messages])
+        return self.get_unread_messages()[0]
 
     def get_message(self, index) -> WorkerMessage:
         """
@@ -120,33 +142,8 @@ class Worker:
         """
         :return: list of messages
         """
-        # TODO: add ability to filter messages by time recieved
+        # TODO: add ability to filter messages by time received
         return self.messages
-
-    def get_next_unread_message(self) -> WorkerMessage:
-        """
-        :return: the next unread message from the list, if one exists
-        Provides the next unread message. If one does not exist, returns None
-        """
-        # TODO: get_next_unread_message() should return next unread message from the list
-        raise NotImplementedError
-
-    def get_queue(self) -> WorkerMessageQueue:
-        """
-        :return: the queue of messages coming from the web worker, if applicable
-        Provides the raw message queue
-        """
-        return self.queue
-
-    def get_next_from_queue(self, block=False, timeout=None):
-        """
-        :param block: whether to wait for a message to come in, defaults False
-        :param timeout: seconds to wait for a message if blocking
-        :return: next message, if available
-        Returns the next message if one is available. Does not block by default; unintentionally blocking can place
-        entire interpreter and possibly webpage into a deadlock. Use carefully!
-        """
-        return self.queue.get(block=block, timeout=timeout)
 
     def send_message(self, message) -> None:
         """
@@ -176,23 +173,51 @@ class Worker:
         else:
             raise WorkerError('Cannot change script after starting!')
 
-    def onmessage(self, event) -> None:
+    def add_to_onmessage(self, function) -> None:
+        """
+        :param function: function or iterable of functions to add to process that runs whenever a message is received
+        from the worker
+        """
+        self.__onmessage_actions += list(function) if isinstance(function, Iterable) else [function]
+
+    def set_onmessage(self, functions) -> None:
+        """
+        :param functions: an iterable of functions to be run whenever a message is received from the worker
+        Sets the actions to be run whenever a message is received. Function to populate messages will be automatically
+        added
+        """
+        to_add = functions if functions else []
+        self.__onmessage_actions = [lambda event: self.messages.append(WorkerMessage(event.data))] + to_add
+
+    def get_onmessage(self) -> list:
+        """
+        :return: all functions to be run during onmessage process
+        Returns all functions that are run when a message is received from the worker. Excludes message collection
+        function that is automatically run whenever list is set
+        """
+        return self.__onmessage_actions[1:]
+
+    def __onmessage(self, event) -> None:
         """
         :param event: message event from the web worker
-        Populates the messages and queue (if provided) with incoming messages whenever one is sent. This method is
-        event-driven and will automatically populate the messages. Must be set BEFORE running if overriding or it will
-        not be picked up by the module.
+        Populates the messages and runs any other provided actions whenever a message is received. New functions can
+        be added with add_to_onmessage or set_onmessage. Any exceptions will be captured and logged to prevent
+        any single action from breaking any processes dependent on the incoming messages
         """
-        if self.queue:
-            self.queue.put(event)
-        self.messages.append(WorkerMessage(event.data))
+        for index, action in enumerate(self.__onmessage_actions):
+            try:
+                action(event)
+            except Exception as e:
+                # all exceptions caught and logged to prevent breaking any other processes reliant on the messages
+                logger.exception(f'onmessage action {index} failed:\n{e}')
 
     def start(self) -> str:
         """
+        :return: the id value for the worker
         Starts the web pywebworker and the message listening service
         """
         self.worker.start()
-        self.worker.pywebworker.onmessage = lambda event: self.onmessage(event)
+        self.worker.pywebworker.onmessage = lambda event: self.__onmessage(event)
         return self.worker.get_id()
 
     def kill(self) -> None:
